aa7b61e221754e8fefd768582e547ea2bfa000c6
Uyên Thy <uynthyee.313@gmail.com>
2025-07-06 02:56:25 +0700

    add login ui

---
+++ FE/src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter } from 'react-router-dom';
+import AppRoutes from './router/routes';
+import Login from './pages/login';
+
+function App() {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+}
+
+
+
+//     </div>
+//     // <BrowserRouter>
+//     //   <AppRoutes />
+//     // </BrowserRouter>
+//   );
+// }
+
+export default App;
---
+++ README.md
@@ -0,0 +1,1 @@
+# Motspetits-FE
